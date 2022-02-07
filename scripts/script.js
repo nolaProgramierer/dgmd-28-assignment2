8285e2a86ae58ed4821b3ff0a22c7d677f98a673
Glenn Langdon <glennlangdon@gmail.com>
2022-02-06 21:48:28 -0600

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+    // On button click make new board
+    document.querySelector("#board-button").addEventListener("click", function () {       
+        console.log("'Show board' button clicked");
+        makeBoard();
+    });
+
+    // 9 squares added to grid container
+    function makeBoard() {
+        // Attributes of empty TTT board
+        var board = [];
+        var numSquares = 9;
+
+        var turns = 0;
+        
+        for (let i = 0; i < numSquares; i++) {
+            let newSquare = document.createElement("div");
+            newSquare.classList.add("ttt-square");
+            const boardDiv = document.getElementById("board-container");
+            boardDiv.appendChild(newSquare);
+            // Board square numbers
+            board.push(i + 1);   
+           
+            // Set data attribute to number of square created
+            newSquare.setAttribute("data-square-num", (i + 1));
+            // Set eventlistener for each square
+            (function (i) {
+                newSquare.addEventListener("click", function() {
+                    // Add function to insert 'X' or 'O'
+                    //this.innerHTML = "<h1>X</h1>";
+                    playGame(this.dataset.squareNum);
+                    console.log(this.dataset.squareNum);
+                });
+            })(i);
+            console.log("Making square number " + (i + 1));
+            // Make checkerboard effect
+            checkerBoard(i, newSquare);          
+        }// End for loop      
+        console.log(board);
+    }// End makeBoard function
+
+    // add X's and 0's
+    var turn = 0;
+    var player1 = [];
+    var player2 = [];
+    function playGame(squareNum) {
+        
+        if (turn % 2 == 0) {
+            this.innerHTML = "<h1>X</h1>";
+            player1.push(squareNum);
+        } else {
+            player2.push(squareNum);
+        }
+        turn ++;
+        console.log(squareNum);
+        console.log("Turn #:" + turn);
+        console.log("Player1: " + player1);
+        console.log("Player2: " + player2);
+    }
+   
+   
+    // Test for winning combination of squares
+    function isWinner(player, board) {
+        if (board[0] == player && board[1] == player % board[2] == player |
+            board[3] == player && board[4] == player & board[5] == player |
+            board[6] == player && board[7] == player & board[8] == player |
+            board[0] == player && board[3] == player & board[6] == player |
+            board[1] == player && board[4] == player & board[7] == player |
+            board[2] == player && board[5] == player & board[8] == player |
+            board[0] == player && board[4] == player & board[8] == player |
+            board[2] == player && board[4] == player & board[6] == player)
+            {
+                console.log("Winner");
+                return true;
+            } else {
+                console.log("No winner");
+                return false;
+            }
+     }
+    
+    // Change background color of square if for loop index is odd
+    function checkerBoard(index, el) {
+        if (index % 2 != 0) {
+            el.style.backgroundColor = "white";
+        }
+        console.log("Background color of this square is white.")
+    }
+    console.log("DOM content parsed and loaded.");
+});//End DOMContentLoaded
