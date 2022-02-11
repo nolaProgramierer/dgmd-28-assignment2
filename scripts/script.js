b7de8e4828417d1ff6b5c0b1fff0db81006aeff4
Glenn Langdon <glennlangdon@gmail.com>
2022-02-11 00:02:40 -0600

    Added styling to X and O

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   
     // On button click make new board
-    document.querySelector("#board-button").addEventListener("click", function () {       
+    document.querySelector("#board-button").addEventListener("click", function () {
         makeBoard();
     });
-
-    document.querySelector("#clear-board").addEventListener("click", function () {       
+    // Reload page
+    document.querySelector("#clear-board").addEventListener("click", function () {
         location.reload();
     });
 
-    // 9 squares added to grid container
+    // 9 squares added to grid container w/ event listener on each square
     function makeBoard() {
         // Attributes of empty TTT board
         var board = [];
@@ -18,52 +18,57 @@ document.addEventListener("DOMContentLoaded", function() {
         var turns = 0;
         
         for (let i = 0; i < numSquares; i++) {
+            // Create board
             let newSquare = document.createElement("div");
             newSquare.classList.add("ttt-square");
             const boardDiv = document.getElementById("board-container");
             boardDiv.appendChild(newSquare);
-            // Board square numbers
-            board.push(i + 1);   
+            // Board square numbers in an array
+            board.push(i + 1);
            
             // Set data attribute to number of square created
             newSquare.setAttribute("data-square-num", (i + 1));
-            // Set eventlistener for each square
+            // Set eventlistener for each square for game play
             (function (i) {
                 newSquare.addEventListener("click", function() {
                     playGame(this, board);
                 });
             })(i);
             console.log("Making square number " + (i + 1));
-            // Make checkerboard effect
-            checkerBoard(i, newSquare);          
-        }// End for loop      
-
+            // Add checkerboard effect
+            checkerBoard(i, newSquare);
+        }// End for loop
     }// End makeBoard function
 
     
     
-    // add X's and 0's
+    // add X's and 0's to board
     var turn = 0;
     var player1 = [];
     var player2 = [];
-    function playGame(newSquare, board) {     
-        if (turn % 2 == 0) {      
+    function playGame(newSquare, board) {
+        if (turn % 2 == 0) {
             player1.push(parseInt(newSquare.dataset.squareNum));
-            newSquare.innerHTML =  "<h1>X</h1>";
-            if(isWinner(player1, board)) {
-                console.log("I won!");
-            };
+            newSquare.classList.add("board-marks");
+            newSquare.innerHTML =  "X";
+            if (isWinner(player1, board)) {
+                console.log("X player won!");
+            }
         } else {
             player2.push(parseInt(newSquare.dataset.squareNum));
-            newSquare.innerHTML =  "<h1>O</h1>";
-            if(isWinner(player2, board)) {
-                console.log("I won!");
-            };
+            newSquare.classList.add("board-marks");
+            newSquare.innerHTML =  "O";
+            if (isWinner(player2, board)) {
+                console.log("O player won!");
+            }
         }
+        // Add conditional for no winner here
         turn ++;
-    } 
+    }
+
+
     // Test for winning combination of squares
-    function isWinner(player, board) {    
+    function isWinner(player, board) {
         if (player.includes(board[0]) && player.includes(board[1]) && player.includes(board[2]) ||
             player.includes(board[3]) && player.includes(board[4]) && player.includes(board[5]) ||
             player.includes(board[6]) && player.includes(board[7]) && player.includes(board[8]) ||
@@ -86,7 +91,6 @@ document.addEventListener("DOMContentLoaded", function() {
         if (index % 2 != 0) {
             el.style.backgroundColor = "white";
         }
-        console.log("Background color of this square is white.")
     }
     console.log("DOM content parsed and loaded.");
 });//End DOMContentLoaded
